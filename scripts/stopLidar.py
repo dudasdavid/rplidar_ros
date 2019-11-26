826b660a8ddd6b3282931cd109df2233dddd807b
david.dudas <david.dudas@outlook.com>
2019-11-26 11:37:46 +0100

    stop lidar script added

---
+++ scripts/stopLidar.py
@@ -0,0 +1,12 @@
+from rplidar import RPLidar
+lidar = RPLidar('/dev/ttyUSB0')
+
+info = lidar.get_info()
+print(info)
+
+health = lidar.get_health()
+print(health)
+
+lidar.stop()
+lidar.stop_motor()
+lidar.disconnect()
